d9de60ccd0e6b0bd0f9ad864960fa12b6a1262ff
KoheiKanagu <kanagu@kingu.dev>
2022-10-04 09:35:33 +0900

    settings page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:brewery/controllers/selected_page_state.dart';
 import 'package:brewery/pages/packages/packages_page.dart';
+import 'package:brewery/pages/settings/settings_page.dart';
 import 'package:brewery/widgets/my_side_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -20,7 +21,7 @@ class HomePage extends HookConsumerWidget {
         body = const PackagesPage();
         break;
       case SelectedPageType.settings:
-        body = Container();
+        body = const SettingsPage();
         break;
     }
     return Material(
---
+++ lib/pages/settings/settings_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class SettingsPage extends HookConsumerWidget {
+  const SettingsPage({
+    super.key,
+  });
+
+  static const path = '/settings';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ListTile(
+            title: const Text('Source'),
+            onTap: () {
+              // TODO
+              launchUrlString('https://github.com/KoheiKanagu');
+            },
+            trailing: const Icon(FontAwesomeIcons.github),
+          ),
+          const Divider(),
+          const ListTile(
+            title: Text('Quit'),
+            onTap: SystemNavigator.pop,
+            trailing: Icon(FontAwesomeIcons.rightFromBracket),
+          )
+        ],
+      ),
+    );
+  }
+}
