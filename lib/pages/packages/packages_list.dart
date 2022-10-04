61778764ce5b549cb7cc659d01a8a7fe327bce9c
KoheiKanagu <kanagu@kingu.dev>
2022-10-04 13:18:08 +0900

    wip

--- lib/pages/packages/packages_list.dart
+++ lib/pages/packages/packages_list.dart
@@ -1,6 +1,7 @@
 import 'package:brewery/models/homebrew/homebrew_info_results.dart';
-import 'package:brewery/pages/packages/package_card.dart';
+import 'package:brewery/pages/packages/packages_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PackagesList extends StatelessWidget {
   const PackagesList(
@@ -13,7 +14,18 @@ class PackagesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: homebrewInfoResults.info.map(PackageCard.new).toList(),
+      children: ListTile.divideTiles(
+        context: context,
+        tiles: homebrewInfoResults.info.map(
+          (e) => ProviderScope(
+            overrides: [
+              packagesListTileTarget.overrideWithValue(e),
+            ],
+            child: const PackagesListTile(),
+          ),
+        ),
+      ).toList(),
+      // children: homebrewInfoResults.info.map(PackageCard.new).toList(),
     );
   }
 }
