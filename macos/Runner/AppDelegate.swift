10b2efb15f336cc36d0bf1249b1621c20777d566
KoheiKanagu <kanagu@kingu.dev>
2025-03-21 15:46:46 +0900

    fix

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import Cocoa
 import FlutterMacOS
 
-@NSApplicationMain
+@main
 class AppDelegate: FlutterAppDelegate {
 
     var statusBar: StatusBarController?
@@ -27,4 +27,3 @@ class AppDelegate: FlutterAppDelegate {
         super.applicationDidFinishLaunching(aNotification)
     }
 }
-
